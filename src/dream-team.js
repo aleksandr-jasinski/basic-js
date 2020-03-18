222cd6de7d4bc8dab5f35c59fa54c6263eba79bb
Aleksandr Jasinski <aleksandr.jasinski@gmail.com>
2020-03-18 18:02:51 +0300

    src/dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,17 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
+module.exports = function createDreamTeam(members) {
+  //throw 'Not implemented';
   // remove line with error and write your code here
+  var team = [];
+  if (Array.isArray(members) == false) {
+    return false;
+  } else {
+    for (let cell of members) {
+      if (typeof cell == 'string') {
+        let letter = cell.trim()[0];
+        team.push(letter.toUpperCase());
+      }
+    }
+    let teamName = team.sort().join('');
+    return teamName;
+  }
 };
