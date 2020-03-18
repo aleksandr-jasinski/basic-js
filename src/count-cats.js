b87d17edbc52d60efd537e4aa97191f0dc425917
Aleksandr Jasinski <aleksandr.jasinski@gmail.com>
2020-03-18 18:02:14 +0300

    src/count-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,12 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  let cats = 0;
+  let cat = '^^';
+  for (let line of backyard) {
+    for (cell of line) {
+      if (cell == cat) {
+        cats++;
+      }
+    }
+  }
+  return cats;
 };
