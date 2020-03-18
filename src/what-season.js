881fd2e0bd90c6180a3de27b8a34893c761cfd85
Aleksandr Jasinski <aleksandr.jasinski@gmail.com>
2020-03-18 22:04:01 +0300

    src/what-season.js dont pass 235

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,27 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+
+  if (date instanceof Date) {
+    let monthNum = date.getMonth();
+    if (monthNum === 0 || monthNum === 1 || monthNum === 11) {
+      return 'winter';
+    }
+    if (monthNum === 2 || monthNum === 3 || monthNum === 4) {
+      return 'spring';
+    }
+    if (monthNum === 5 || monthNum === 6 || monthNum === 7) {
+      return 'summer';
+    }
+    if (monthNum === 8 || monthNum === 9 || monthNum === 10) {
+      return 'autumn';
+    }
+  }
+
+  else if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+
+  else {
+    throw new Error();
+  }
+
 };
