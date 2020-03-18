2c1344b3478f0a95b79d78c82bd1e90b03b6b064
Aleksandr Jasinski <aleksandr.jasinski@gmail.com>
2020-03-18 22:05:07 +0300

    src/transform-array.js dont pass 44

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,32 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
+module.exports = function transform(arr) {
+    //throw 'Not implemented';
     // remove line with error and write your code here
+    if (Array.isArray(arr)) {
+        var arrTransformed = [];
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === '--double-next') {
+                if (arr[i + 1] !== undefined) {
+                    arrTransformed.push(arr[i + 1]);
+                }
+            }
+            else if (arr[i] === '--double-prev') {
+                arrTransformed.push(arr[i - 1]);
+            }
+            else if (arr[i] === '--discard-next') {
+                i++;
+            }
+            else if (arr[i] === '--discard-prev') {
+                arrTransformed.pop();
+            }
+            else {
+                arrTransformed.push(arr[i]);
+            }
+        }
+
+        return arrTransformed;
+    }
+    else {
+        throw Error();
+    }
+
 };
