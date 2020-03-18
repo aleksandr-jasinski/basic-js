d60ac9a1d50b004e67019dca43e75258dfa24936
Aleksandr Jasinski <aleksandr.jasinski@gmail.com>
2020-03-18 18:10:28 +0300

    src/extended-repeater.js dont pass 39 & 159

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,62 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    if (str === null || str === undefined) {
+        return false;
+    }
+    else if (options.repeatTimes === undefined) {
+        return false;
+    }
+    else if (options.addition !== undefined && options.additionRepeatTimes === undefined) {
+        return false;
+    }
+    else {
+        str.toString();
+
+        let repeat = options.repeatTimes;
+
+        if (options.separator === undefined) {
+            var sep = '+';
+        } else {
+            var sep = options.separator;
+        }
+
+        let add = options.addition;
+        if (add !== undefined) {
+            add.toString();
+        }
+
+        if (options.additionRepeatTimes === undefined) {
+            var addRepeat = 1;
+        } else {
+            var addRepeat = options.additionRepeatTimes;
+        }
+
+
+        if (options.additionSeparator === undefined) {
+            var addSep = '|';
+        } else {
+            var addSep = options.additionSeparator;
+        }
+
+
+        let repeating = [];
+
+        for (let r = 0; r < repeat; r++) {
+            repeating.push(str);
+
+            for (let a = 0; a < addRepeat - 1; a++) {
+                repeating.push(add);
+                repeating.push(addSep);
+
+            }
+            repeating.push(add);
+            repeating.push(sep);
+        }
+
+        repeating.pop();
+
+        let repeatedStr = repeating.join('');
+        return repeatedStr;
+
+    }
+
 };
-  
