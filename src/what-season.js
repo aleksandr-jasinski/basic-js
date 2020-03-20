cea6395bc85fb0797407c9246ae8712051fbc5a7
Aleksandr Jasinski <aleksandr.jasinski@gmail.com>
2020-03-20 15:05:06 +0300

    fix src/what-season.js

--- src/what-season.js
+++ src/what-season.js
@@ -1,27 +1,31 @@
 module.exports = function getSeason(date) {
 
-  if (date instanceof Date) {
-    let monthNum = date.getMonth();
-    if (monthNum === 0 || monthNum === 1 || monthNum === 11) {
-      return 'winter';
-    }
-    if (monthNum === 2 || monthNum === 3 || monthNum === 4) {
-      return 'spring';
-    }
-    if (monthNum === 5 || monthNum === 6 || monthNum === 7) {
-      return 'summer';
-    }
-    if (monthNum === 8 || monthNum === 9 || monthNum === 10) {
-      return 'autumn';
-    }
-  }
-
-  else if (!date) {
+  if (!date) {
     return 'Unable to determine the time of year!';
   }
 
-  else {
+  else if (isNaN(date) || isNaN(Date.parse(date))) {
     throw new Error();
+  }
+
+  else {
+
+    let monthNum = date.getMonth();
+
+    if (monthNum === 11 || monthNum === 0 || monthNum === 1) {
+      return 'winter';
+    }
+    else if (monthNum === 2 || monthNum === 3 || monthNum === 4) {
+      return 'spring';
+    }
+    else if (monthNum === 5 || monthNum === 6 || monthNum === 7) {
+      return 'summer';
+    }
+    else //if (monthNum === 8 || monthNum === 9 || monthNum === 10) 
+    {
+      return 'autumn';
+    }
+
   }
 
 };
