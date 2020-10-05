9ab62dc72e1c4377417c4699d67e4415ca3f2abb
Aleksandr Jasinski <aleksandr.jasinski@gmail.com>
2020-10-05 18:43:03 +0300

    fix: improve solution for transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -3,24 +3,28 @@ module.exports = function transform(arr) {
   // remove line with error and write your code here
   if (Array.isArray(arr)) {
     let arrTransformed = [];
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] === '--double-next') {
-        if (arr[i + 1] !== undefined) {
-          arrTransformed.push(arr[i + 1]);
-        }
-      } else if (arr[i] === '--double-prev') {
-        if (arr[i - 1] !== undefined) {
+    for (let i = 0; i < arr.length; i += 1) {
+      if (arr[i + 1] !== undefined && arr[i] === '--double-next') {
+        arrTransformed.push(arr[i + 1]);
+      } else if (i > 0 && arr[i] === '--double-prev') {
+        if (arr[i - 2] !== '--discard-next') {
           arrTransformed.push(arr[i - 1]);
         }
-      } else if (arr[i] === '--discard-next') {
-        i++;
-      } else if (arr[i] === '--discard-prev') {
-        arrTransformed.pop();
-      } else {
+      } else if (arr[i + 1] !== undefined && arr[i] === '--discard-next') {
+        i += 1;
+      } else if (i > 0 && arr[i] === '--discard-prev') {
+        if (i >= 2 && arr[i - 2] !== '--discard-next') {
+          arrTransformed.pop();
+        }
+      } else if (
+        arr[i] !== '--double-prev' &&
+        arr[i] !== '--discard-prev' &&
+        arr[i] !== '--double-next' &&
+        arr[i] !== '--discard-next'
+      ) {
         arrTransformed.push(arr[i]);
       }
     }
-
     return arrTransformed;
   } else {
     throw Error();
