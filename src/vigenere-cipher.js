813b585cc876ca476e7f1479f3e28fc3865796ff
Aleksandr Jasinski <aleksandr.jasinski@gmail.com>
2020-10-31 20:10:15 +0300

    feat: add Vigenere cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,64 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+  }
+  encrypt(message, key) {
+
+    if (!message || !key) {
+      throw new Error;
+    } else {
+      let result = '';
+      let MES = message.toString().toUpperCase();
+      let KEY = key.toString().toUpperCase();
+      const maxLen = Math.max(MES.length, KEY.length);
+      const abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+      for (let i = 0; i < maxLen; i += 1) {
+        let ms = MES[((i >= MES.length) ? i % MES.length : i)];
+        let msi = abc.indexOf(ms);
+        let sign;
+        if (msi === -1) {
+          sign = ms;
+        } else {
+          let ks = KEY[((i >= KEY.length) ? i % KEY.length : i)];
+          let ksi = abc.indexOf(ks);
+          sign = abc[(((26 + (msi + ksi)) % 26))];
+        }
+        result += sign;
+      }
+      return (this.isDirect ? result : result.split('').reverse().join(''));
+    }
+
+  }
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error;
+    } else {
+      let result = '';
+      let MES = message.toString().toUpperCase();
+      let KEY = key.toString().toUpperCase();
+      const maxLen = Math.max(MES.length, KEY.length);
+      const abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+      for (let i = 0; i < maxLen; i += 1) {
+        let ms = MES[((i >= MES.length) ? i % MES.length : i)];
+        let msi = abc.indexOf(ms);
+        let sign;
+        if (msi === -1) {
+          sign = ms;
+        } else {
+          let ks = KEY[((i >= KEY.length) ? i % KEY.length : i)];
+          let ksi = abc.indexOf(ks);
+          sign = abc[(((26 + (msi - ksi)) % 26))];
+        }
+        result += sign;
+      }
+      return (this.isDirect ? result : result.split('').reverse().join(''));
+    }
+
   }
 }
 
