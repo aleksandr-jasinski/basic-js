d0b95d57218ae8f6358c9c83bd1c4a2b1ce9cdef
Aleksandr Jasinski <aleksandr.jasinski@gmail.com>
2020-11-01 11:49:49 +0300

    feat: add <<simple chain>> solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,45 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  'chain': [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    // возвращает текущую длину цепи в виде числа
+    return this.chain.length;
   },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value='( )') {
+    // добавляет звено, содержащее строковое представление value к цепочке
+    // Если addLink вызван без аргументов,
+    // он добавляет пустое звено ('( )') в цепочку.
+    this.chain.push(`( ${value} )`);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    // удаляет звено цепи, находящееся в заданном положении;
+    // Если removeLink принимает
+    // не число, или дробное число, или ссылающуюся на несуществующее звено,
+    // он должен выбросить ошибку.
+    if(Number.isInteger(position) && (0 < position || position <= this.chain.length)) {
+      this.chain.splice(position - 1, 1);
+      return this;
+    } else {
+      throw new Error;
+    }
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    // разворачивает цепь задом наперед;
+    if (this.chain !== undefined) {
+      this.chain.reverse();
+    }
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    // завершает цепь и возвращает ее.
+    // После вызова метода finishChain,
+    // существующая на данный момент цепь должна быть удалена,
+    // как и в случае, если была выброшена ошибка.
+    const output = this.chain.join('~~');
+    this.chain = [];
+    return output;
   }
 };
 
