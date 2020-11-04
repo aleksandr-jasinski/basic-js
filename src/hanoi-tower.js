df96322082154fc1deb2b79fc41e3e5076b69b3d
Aleksandr Jasinski <aleksandr.jasinski@gmail.com>
2020-11-04 10:02:15 +0300

    refactor: refactor Hanoi Tower solving with formula

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,9 +1,9 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-  //throw 'Not implemented';
-  // remove line with error and write your code here
-  let turnsNum = 2 ** disksNumber - 1;
 
-  let secondsNum = Math.floor(turnsNum / (turnsSpeed / 3600));
+  // solving with formula
+  const turns = 2 ** disksNumber - 1;
 
-  return { turns: turnsNum, seconds: secondsNum };
+  const seconds = Math.floor(turns / (turnsSpeed / 3600));
+
+  return { turns, seconds };
 };
