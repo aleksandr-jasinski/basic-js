699be4d7a5d543c91729602ca106d9ff8362ad25
Aleksandr Jasinski <aleksandr.jasinski@gmail.com>
2020-11-04 14:18:31 +0300

    refactor: refactor Extended Repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,60 +1,42 @@
 module.exports = function repeater(str, options) {
-  if (str === undefined) {
-    return false;
+  if (str === undefined) {return false;}
+
+  if (str === null) {
+    str = 'null';
   } else {
-    if (str === null) {
-      str = 'null';
-    } else {
-      str.toString();
-    }
-
-    if (options.repeatTimes === undefined) {
-      var repeat = 1;
-    } else {
-      var repeat = options.repeatTimes;
-    }
-
-    if (options.separator === undefined) {
-      var sep = '+';
-    } else {
-      var sep = options.separator;
-    }
-
-    let add = options.addition;
-    if (add === null) {
-      add = 'null';
-    } else if (add !== undefined) {
-      add.toString();
-    }
-
-    if (options.additionRepeatTimes === undefined) {
-      var addRepeat = 1;
-    } else {
-      var addRepeat = options.additionRepeatTimes;
-    }
-
-    if (options.additionSeparator === undefined) {
-      var addSep = '|';
-    } else {
-      var addSep = options.additionSeparator;
-    }
-
-    let repeating = [];
-
-    for (let r = 0; r < repeat; r++) {
-      repeating.push(str);
-
-      for (let a = 0; a < addRepeat - 1; a++) {
-        repeating.push(add);
-        repeating.push(addSep);
-      }
-      repeating.push(add);
-      repeating.push(sep);
-    }
-
-    repeating.pop();
-
-    let repeatedStr = repeating.join('');
-    return repeatedStr;
+    str.toString();
   }
+
+  let add = options.addition;
+  if (add === null) {
+    add = 'null';
+  } else if (add !== undefined) {
+    add.toString();
+  }
+
+  let repeat = options.repeatTimes  === undefined ? 1 : options.repeatTimes;
+
+  let sep = options.separator === undefined ? '+' : options.separator;
+
+  let addRepeat = options.additionRepeatTimes === undefined ? 1 : options.additionRepeatTimes;
+
+  let addSep = options.additionSeparator === undefined ? '|' : options.additionSeparator;
+
+  const repeating = [];
+
+  for (let r = 0; r < repeat; r += 1) {
+    repeating.push(str);
+
+    for (let a = 0; a < addRepeat - 1; a += 1) {
+      repeating.push(add);
+      repeating.push(addSep);
+    }
+    repeating.push(add);
+    repeating.push(sep);
+  }
+
+  repeating.pop();
+
+  return repeating.join('');
+
 };
